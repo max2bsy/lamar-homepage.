c869459bc7e42d62d642bf1b7fab3b72005d2ef6
max2bsy <100826744+max2bsy@users.noreply.github.com>
2024-12-13 12:36:30 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+myAudio = document.getElementById("my-click");
+
+function weaknessPage() {
+  var url = "https://ctgr3h.csb.app/";
+  window.location = url;
+}
+function wickednessPage() {
+  var url = "https://3gvwp7.csb.app/";
+  window.location = url;
+}
